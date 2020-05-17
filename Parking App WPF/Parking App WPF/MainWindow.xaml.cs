9266b2c0b20a5c3eec86d7708e72c646512bf8ac
Martin Rylund <martin@rylund.eu>
2020-05-17 23:49:03 +0200

    Added authentication when pressing login, admin and resident accounts

--- Parking App WPF/Parking App WPF/MainWindow.xaml.cs
+++ Parking App WPF/Parking App WPF/MainWindow.xaml.cs
@@ -30,7 +30,7 @@ namespace Parking_App_WPF
         {
             InitializeComponent();
             mysql = new MySQL();
-            //string query = String.Format("INSERT INTO users (Name, Room, username, password) VALUES ('Martin Rylund', '228', '{0}', '{1}')", "sup3", Encrypt("sup3"));
+            //string query = String.Format("INSERT INTO users (Name, Room, username, password, rank) VALUES ('Martin Rylund', '228', '{0}', '{1}', 'Admin')", "admin", Encrypt("admin"));
             //mysql.Execute(query);
         }
 
@@ -45,17 +45,18 @@ namespace Parking_App_WPF
             }
         }
 
-        private void AuthenticateUser(string username, string password)
+        private Tuple<bool, string> AuthenticateUser(string username, string password)
         {
             string shaPassword = Encrypt(password);
             string query = String.Format("SELECT * FROM users WHERE Username='{0}' AND password='{1}' LIMIT 1", username, shaPassword);
             DataTable dt = mysql.Select(query);
             if (dt.Rows.Count != 1) {
                 Debug.WriteLine("No user found with provided credentials");
-                return;
+                return Tuple.Create(false, "");
             }
             DataRow dr = dt.Rows[0];
-            Debug.WriteLine(dr["Name"]);
+            string rank = (String)dr["Rank"];
+            return Tuple.Create(true, rank);
         }
 
         private void login(object sender, RoutedEventArgs e)
@@ -65,9 +66,19 @@ namespace Parking_App_WPF
             string username = usnTextBox.Text;
             string password = pwTextBox.Text;
 
-            AuthenticateUser(username, password);
-            //this.Close();
-            //obj.Show();
+            var info = AuthenticateUser(username, password);
+
+            if (!info.Item1) return;
+
+            if (info.Item2 == "Admin")
+            {
+                
+            } else
+            {
+                LandingPage l = new LandingPage();
+                l.Show();
+            }
+            this.Close();
         }
 
         private void username_txtbx_TextChanged(object sender, TextChangedEventArgs e)
