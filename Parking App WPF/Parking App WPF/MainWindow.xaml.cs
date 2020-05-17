684b4bc8c365f71736fa106e195555dfd6bbe319
Martin Rylund <martin@rylund.eu>
2020-05-17 23:19:07 +0200

    Fixed merge conflict between UI and backend

--- Parking App WPF/Parking App WPF/MainWindow.xaml.cs
+++ Parking App WPF/Parking App WPF/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
 
 namespace Parking_App_WPF
 {
@@ -20,9 +23,45 @@ namespace Parking_App_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private MySQL mysql;
+
         public MainWindow()
         {
             InitializeComponent();
+            mysql = new MySQL();
+            //string query = String.Format("INSERT INTO users (Name, Room, username, password) VALUES ('Martin Rylund', '228', '{0}', '{1}')", "sup3", Encrypt("sup3"));
+            //mysql.Execute(query);
+        }
+
+        private string Encrypt(string s)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] sourceBytes = Encoding.UTF8.GetBytes(s);
+                byte[] hashBytes = sha256Hash.ComputeHash(sourceBytes);
+                string encrypted = BitConverter.ToString(hashBytes).Replace("-", String.Empty);
+                return encrypted;
+            }
+        }
+
+        private void AuthenticateUser(string username, string password)
+        {
+            Debug.WriteLine("TRYING TO FIND USERNAME");
+            string shaPassword = Encrypt(password);
+            string query = String.Format("SELECT * FROM users WHERE Username='{0}' AND password='{1}' LIMIT 1", username, shaPassword);
+            MySqlDataReader reader = mysql.Select(query);
+            while (reader.Read())
+            {
+                Debug.WriteLine(reader["Name"]);
+            }
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            AuthenticateUser("sup2", "sup2");
+            test obj = new test();
+            //this.Close();
+            //obj.Show();
         }
 
         private void username_txtbx_TextChanged(object sender, TextChangedEventArgs e)
---
+++ Parking App WPF/Parking App WPF/MySQL.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using MySql.Data.MySqlClient;
+using System.Diagnostics;
+using System.Data;
+
+namespace Parking_App_WPF
+{
+    class MySQL
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        //Constructor
+        public MySQL()
+        {
+            Initialize();
+        }
+
+        //Initialize values
+        private void Initialize()
+        {
+            server = "rylund.dev";
+            database = "u748359586_cskarp";
+            uid = "u748359586_cskarp";
+            password = "cskarp";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";" + "CharSet=utf8; pooling=false;";
+
+            connection = new MySqlConnection(connectionString);
+        }
+
+
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        Debug.WriteLine("Cannot connect to server.  Contact administrator");
+                        break;
+
+                    case 1045:
+                        Debug.WriteLine("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        public void Execute(string query)
+        {
+            if (this.OpenConnection() == true)
+            {
+                Debug.WriteLine(query);
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                cmd.ExecuteNonQuery();
+                this.CloseConnection();
+            }
+        }
+
+
+        //Select statement
+        public MySqlDataReader Select(string query)
+        {
+            //Open connection
+            if (this.OpenConnection() == true)
+            {
+                //Create Command
+                MySqlCommand cmd = new MySqlCommand(query + ";CharSet=utf8;", connection);
+                //Create a data reader and Execute the command
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+                return dataReader;
+            }
+            return null;
+        }
+    }
+}
