5f7847c3a22f2f0da78bd4a68f1efe28ae04ecd1
Martin Rylund <martin@rylund.eu>
2020-05-18 21:57:54 +0200

    Changes in adding guest passes
    License plates now gets added to the database
    If a pass is about to expire (1h left) it will automatically renew it

--- Parking App WPF/Parking App WPF/User.cs
+++ Parking App WPF/Parking App WPF/User.cs
@@ -57,10 +57,11 @@ namespace Parking_App_WPF
 
         public (bool, string) addGuest(string LicensePlate)
         {
+            string msg;
             string LicensePattern = "[A-Za-z][A-Za-z]\\s?[0-9][0-9]\\s?[0-9][0-9][0-9]";
             if (!Regex.Match(LicensePlate, LicensePattern).Success)
             {
-                string msg = "The license plate does not match the correct format";
+                msg = "The license plate does not match the correct format";
                 Debug.WriteLine(msg);
                 return (false, msg);
             }
@@ -70,18 +71,37 @@ namespace Parking_App_WPF
             LicensePlate = LicensePlate.Insert(5, " ");
             LicensePlate = LicensePlate.ToUpper();
 
-            string query = String.Format("SELECT LicensePlate, created FROM guests WHERE LicensePlate='{0}' AND created > TIMESTAMPADD(DAY, -1, NOW())", LicensePlate);
+            string query = String.Format("SELECT ID, LicensePlate, created FROM guests WHERE LicensePlate='{0}' AND created > TIMESTAMPADD(DAY, -1, NOW())", LicensePlate);
             DataTable dt = mysql.Select(query);
             if (dt.Rows.Count > 0)
             {
-                string msg = "An active guest pass with given license already exist";
+                DataRow dr = dt.Rows[0];
+                if ((DateTime.Now - (DateTime)dr["created"]).TotalHours > 23)
+                {
+                    query = String.Format("UPDATE guests SET created=current_timestamp() WHERE ID='{0}' AND Resident='{1}'", Convert.ToInt32(dr["ID"]), UID);
+                    mysql.Execute(query);
+                    msg = String.Format("Updated expiration date of guest pass for license plate {0}", LicensePlate);
+                    return (true, msg);
+                }
+                msg = "An active guest pass with given license already exist";
                 Debug.WriteLine(msg);
                 return (false, msg);
             }
 
+            query = String.Format("SELECT COUNT(*) AS NumCars FROM guests WHERE Resident='{0}' AND created > TIMESTAMPADD(DAY, -1, NOW())", UID);
+            dt = mysql.Select(query);
+            int NumCars = dt.Rows[0]["NumCars"] == DBNull.Value ? 0 : Convert.ToInt32(dt.Rows[0]["NumCars"]);
+            if (NumCars >= 5)
+            {
+                msg = "You have more than the max allowed of guest vehicles";
+                Debug.WriteLine(msg);
+                return (false, msg);
+            }
+
+            query = string.Format("INSERT INTO guests(LicensePlate, Resident) VALUES('{0}', '{1}')", LicensePlate, UID);
+            mysql.Execute(query);
             Debug.WriteLine(LicensePlate);
             return (false, "hej");
-
         }
 
     }
