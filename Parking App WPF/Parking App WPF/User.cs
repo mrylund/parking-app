ab00c38a8d69b079e857351c3f9c739c4afc4b5d
Martin Rylund <martin@rylund.eu>
2020-05-18 14:45:14 +0200

    Started on user class

---
+++ Parking App WPF/Parking App WPF/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking_App_WPF
+{
+    class User
+    {
+        private string username, name, room, licenseplate, rank;
+
+        public User(string username, string name, string room, string licenseplate, string rank)
+        {
+            this.username = username;
+            this.name = name;
+            this.room = room;
+            this.licenseplate = licenseplate;
+            this.rank = rank;
+        }
+    }
+}
