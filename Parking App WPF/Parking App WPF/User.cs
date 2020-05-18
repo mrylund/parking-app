2641399e29253fdfbfc1f53b1e25dd49dc3bb771
Martin Rylund <martin@rylund.eu>
2020-05-18 19:43:35 +0200

    Only select guest vehicles newer than 1 day old

--- Parking App WPF/Parking App WPF/User.cs
+++ Parking App WPF/Parking App WPF/User.cs
@@ -34,7 +34,7 @@ namespace Parking_App_WPF
 
         private void fetchCars()
         {
-            string query = String.Format("SELECT LicensePlate, created FROM guests WHERE Resident='{0}'", UID);
+            string query = String.Format("SELECT LicensePlate, created FROM guests WHERE Resident={0} AND created > TIMESTAMPADD(DAY, -1, NOW())", UID);
             DataTable dt = mysql.Select(query);
             if (dt.Rows.Count > 1)
             {
@@ -44,8 +44,10 @@ namespace Parking_App_WPF
             foreach(DataRow row in dt.Rows)
             {
                 string license = row["LicensePlate"] == DBNull.Value ? string.Empty : (String)row["LicensePlate"];
-                DateTime created = row["created"] == DBNull.Value ? DateTime.Parse("0") : DateTime.Parse((String)row["created"]);
+                DateTime created = row["created"] == DBNull.Value ? DateTime.Parse("0") : (DateTime)row["created"];
                 guests.Add((license, created));
+
+                Debug.WriteLine(String.Format("License Plate: {0}    Created: {1}", license, created));
             }
         }
     }
